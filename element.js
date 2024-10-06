1e5ea79bb83160b6e9142ed671b1ec6eb04e8672
Danny Engelman <danny@engelman.nl>
2024-10-06 17:33:11 +0200

    v4

--- element.js
+++ element.js
@@ -4,15 +4,14 @@ customElements.define(
     connectedCallback(name) {
       setTimeout(() => {
         this.attachShadow({ mode: "open" }).innerHTML =
-          "<tag part=tag-name>" + // let the user style shadowDOM with ::part(tag-name)
+          "<t part=tag-name>" + // let the user style shadowDOM with ::part(tag-name)
           "<span part=span>\u2039</span>" + // opening bracket, user can style with ::part(span)
-          "<name part=name>" +
+          "<n part=name>" +
           (name = this.innerHTML) +
-          "</name>" +
+          "</n>" +
           "<span part=span>\u203A</span>" +
-          "<style>:host{white-space:nowrap}" +
-          ":host(:hover){cursor:pointer}" +
-          "part{line-height:0;font-size:1.7em;position:relative;top:.07em";
+          "<style>:host{white-space:nowrap;cursor:pointer}" +
+          "span{line-height:0;font-size:1.7em;position:relative;top:.07em";
         this.onclick = (e) =>
           (document.location = "https://" + name.trim() + ".github.io");
       });
