564630229eb648abdaf159b0fd88b0deaa43dd1d
Danny Engelman <danny@engelman.nl>
2024-10-07 15:23:19 +0200

    v5

--- element.js
+++ element.js
@@ -1,19 +1,37 @@
 customElements.define(
   "tag-name",
   class extends HTMLElement {
-    connectedCallback(name) {
+    connectedCallback(tag = this.getAttribute("tag"), name) {
       setTimeout(() => {
         this.attachShadow({ mode: "open" }).innerHTML =
-          "<t part=tag-name>" + // let the user style shadowDOM with ::part(tag-name)
-          "<span part=span>\u2039</span>" + // opening bracket, user can style with ::part(span)
-          "<n part=name>" +
-          (name = this.innerHTML) +
+          "<t part=all>" +
+          // opening <
+          "<span part=span>\u2039</span>" +
+          // name
+          "<n part=tag-name>" +
+          (tag || (name = this.innerHTML)) +
+          (tag ? " <a>" + (this.getAttribute("attrs") || "") + "</a>" : "") +
           "</n>" +
+          // closing >
           "<span part=span>\u203A</span>" +
+          (tag
+            ? "<n part=name>" +
+              this.innerHTML +
+              "</n>" +
+              // opening < with /
+              "<span part=span>\u2039</span><b part=slash>/</b>" +
+              "<n part=tag-name>" +
+              tag +
+              "</n>" +
+              // closing >
+              "<span part=span>\u203A</span>"
+            : "") +
           "<style>:host{white-space:nowrap;cursor:pointer}" +
           "span{line-height:0;font-size:1.7em;position:relative;top:.07em";
         this.onclick = (e) =>
-          (document.location = "https://" + name.trim() + ".github.io");
+          (document.location =
+            this.getAttribute("url") ||
+            "https://" + name.trim() + ".github.io");
       });
     }
   }
