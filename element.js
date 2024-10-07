f7accbeb2b3c334e4952ae08bce5ac2be7eeff60
Danny Engelman <danny@engelman.nl>
2024-10-07 15:40:43 +0200

    v6

--- element.js
+++ element.js
@@ -1,3 +1,23 @@
+/**
+ * Custom element definition for "tag-name".
+ *
+ * This custom element displays a tag name with optional attributes and a URL redirection on click.
+ *
+ * @class
+ * @extends HTMLElement
+ *
+ * @method connectedCallback
+ * @param {string} [tag] - The tag name to display. Defaults to the value of the "tag" attribute.
+ * @param {string} [name] - The name to use for URL redirection. Defaults to the innerHTML of the element.
+ *
+ * @description
+ * When the element is connected to the DOM, it sets up its shadow DOM with the appropriate HTML structure
+ * and styles. It also sets up an onclick event handler that redirects the user to a URL based on the "url"
+ * attribute or the name of the element.
+ *
+ * @example
+ * <tag-name tag="example" attrs="some-attributes" url="https://example.com">Example</tag-name>
+ */
 customElements.define(
   "tag-name",
   class extends HTMLElement {
@@ -16,7 +36,7 @@ customElements.define(
           "<span part=span>\u203A</span>" +
           (tag
             ? "<n part=name>" +
-              this.innerHTML +
+              (name = this.innerHTML) +
               "</n>" +
               // opening < with /
               "<span part=span>\u2039</span><b part=slash>/</b>" +
